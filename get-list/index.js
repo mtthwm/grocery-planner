3ee4af142eb3a9d87be2b2dad12decf485e19510
Matthew Morales <mtthwmrls9@gmail.com>
2022-07-23 12:40:48 -0600

    Finished get list function

--- get-list/index.js
+++ get-list/index.js
@@ -4,8 +4,53 @@ const dbContext = require('../shared/dbContext');
 const validation = require('../shared/validation');
 
 module.exports = async function (context, req) {
+    const accessToken = req.body.accessToken;
+
+    const userId = await validation.validateUser(accessToken);
+
+    if (!userId)
+    {
+        context.res = {
+            status: 401
+        }
+    }
+
     const {endpoint, key} = config;
 
     const cosmosClient = new CosmosClient({endpoint, key});
     const container = await dbContext.setupContainer(cosmosClient, config);
+
+    const list = await getList(container, userId);
+
+    if (list) {
+        context.res = {
+            status: 200,
+            body: {
+                list,
+            }
+        }
+    } else {
+        context.res = {
+            status: 404,
+            body: {
+                errors: [
+                    'List not found'
+                ]
+            }
+        }
+    }
 }
+
+const getList = async (container, userId) => {
+    const querySpec = {
+        query: `SELECT * from ${container.id} l WHERE l.user="${userId}"`,
+    }
+
+    const {resources: items} = await container.items.query(querySpec).fetchAll();
+
+    if (items.length > 0) {
+        return items[0];
+    } else {
+        return null;
+    }    
+};
