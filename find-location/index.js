fd63066233ea74da3838381f6445355252625de4
Matthew Morales <mtthwmrls9@gmail.com>
2022-08-12 14:10:42 -0600

    Improving error handling

--- find-location/index.js
+++ find-location/index.js
@@ -1,10 +1,20 @@
 const fetch = require('node-fetch');
+const validation = require('../shared/validation');
 
 module.exports = async function (context, req) {
     const zipCode = req.query.zipCode;
     const accessToken = req.body.accessToken;
     const resultCount = 50;
     const excludedChains = ['SHELL COMPANY', 'JEWELRY']
+
+    const userId = await validation.validateUser(accessToken);
+    if (!userId)
+    {
+        context.res = {
+            status: 401
+        }
+        return; 
+    }
 
     const response = await fetch(`https://${process.env.KROGER_API_DOMAIN}/v1/locations?filter.zipCode.near=${zipCode}&filter.limit=${resultCount}`, {
         method: 'GET',
@@ -22,6 +32,7 @@ module.exports = async function (context, req) {
                 error: 'Token expired or Invalid'
             }
         }
+        return;
     } 
     else 
     {
@@ -30,5 +41,6 @@ module.exports = async function (context, req) {
             status: 200, /* Defaults to 200 */
             body: locations,
         };
+        return;
     }
 }
--- search-products/index.js
+++ search-products/index.js
@@ -1,10 +1,20 @@
 const fetch = require('node-fetch');
+const validation = require('../shared/validation');
 
 module.exports = async function (context, req) {
     const accessToken = req.body.accessToken;
     const searchTerm = req.query.term;
     const locationId = req.query.locationId;
     const resultCount = 50;
+
+    const userId = await validation.validateUser(accessToken);
+    if (!userId)
+    {
+        context.res = {
+            status: 401
+        }
+        return; 
+    }
 
     const response = await fetch(`https://${process.env.KROGER_API_DOMAIN}/v1/products?filter.term=${searchTerm}&filter.locationId=${locationId}&filter.limit=${resultCount}`, {
         method: 'GET',
